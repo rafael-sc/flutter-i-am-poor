1dc28393d01cfbebc8e6dab9938617bdbfb865f8
Rafael Costa <rafael.costa.ti@gmail.com>
2020-11-24 20:13:45 -0300

    Creating Home Class and DefaultAppBar Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('I am poor'),
-        backgroundColor: Colors.amber,
-      ),
-      body: Center(
-        child: Image(
-          image: NetworkImage(
-              'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg'),
-        ),
-      ),
-    ),
-  ));
+  runApp(Home().get());
 }
 
+class Home {
+  MaterialApp get() {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am poor'),
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DefaultAppBar {
+  AppBar get() {
+    return AppBar(
+      title: Text('I am poor'),
+      backgroundColor: Colors.amber,
+    );
+  }
+}
