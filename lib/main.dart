dba25b855e5970f06e82f9ca23d1d09cffdffc4a
Rafael Costa <rafael.costa.ti@gmail.com>
2020-11-24 20:38:34 -0300

    Adding OKToast and GestureDetector to wrap coal image

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:oktoast/oktoast.dart';
 
 void main() {
   runApp(Home().get());
 }
 
 class Home {
-  MaterialApp get() {
-    return MaterialApp(
+  OKToast get() {
+    return OKToast(
+        child: MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('I am poor'),
-          backgroundColor: Colors.amber,
+          backgroundColor: Colors.brown[900],
         ),
+        backgroundColor: Colors.brown[700],
         body: Center(
-          child: Image(
-            image: AssetImage('images/coal.png'),
+          child: GestureDetector(
+            onTap: () {
+              showToast(
+                "I am poor",
+                position: ToastPosition.bottom,
+              );
+            },
+            child: Image(
+              image: AssetImage('images/coal.png'),
+            ),
           ),
         ),
       ),
-    );
+    ));
   }
 }
 
